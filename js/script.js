f22b52d27fee196d5cb54fb798c7bded9b2b35f1
deadleaves15 <69413633+deadleaves15@users.noreply.github.com>
2023-01-10 19:41:23 -0700

    Changes vanilla querySelector list to jQuery code

--- js/script.js
+++ js/script.js
@@ -1,6 +1,5 @@
 function newItem(){
 
-    //javascript
     //1. Adding a new item to the list of items: 
        let li = document.createElement("li");
        let inputValue = document.getElementById("input").value;
@@ -10,8 +9,7 @@ function newItem(){
        if (inputValue === '') {
          alert("You must write something!");
        } else {
-         let list = document.querySelector('#list');
-         list.appendChild(li);
+        $('#list').append(li);
        }
     
      //2. Crossing out an item from the list of items:
